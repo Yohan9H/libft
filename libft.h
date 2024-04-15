fbb06f50e6542aa9da25f1ed2de4899ed225f1ca
Yohan9H <yhurtebize58@gmail.com>
2024-04-15 11:51:43 +0200

    reorga fichier pour makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,23 @@
 /*   By: yohan.h <yohan.h@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:23:30 by yohan.h           #+#    #+#             */
-/*   Updated: 2024/04/09 18:24:03 by yohan.h          ###   ########.fr       */
+/*   Updated: 2024/04/15 11:50:48 by yohan.h          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+
+
+
+
+
+
+
+#endif
